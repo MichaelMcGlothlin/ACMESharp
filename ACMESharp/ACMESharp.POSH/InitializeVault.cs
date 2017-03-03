dc559d9de4b6e13e284f4414650866e8ef229c46
Michael McGlothlin <michael@kavlon.com>
2017-03-03 12:13:30 -0600

    Code cleanup..

--- ACMESharp/ACMESharp.POSH/InitializeVault.cs
+++ ACMESharp/ACMESharp.POSH/InitializeVault.cs
@@ -1,64 +1,61 @@
 ﻿using ACMESharp.Vault.Model;
 using ACMESharp.Vault.Util;
+using System;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
 using System.Management.Automation;
 
 namespace ACMESharp.POSH {
- [Cmdlet ( VerbsData.Initialize, "Vault", DefaultParameterSetName = PSET_BASE_SERVICE )]
+ [Cmdlet ( VerbsData.Initialize, @"Vault", DefaultParameterSetName = PSET_BASE_SERVICE )]
  public class InitializeVault : Cmdlet {
-  public const System.String PSET_BASE_SERVICE = "BaseService";
-  public const System.String PSET_BASE_URI = "BaseUri";
+  public const String PSET_BASE_SERVICE = @"BaseService";
+  public const String PSET_BASE_URI = @"BaseUri";
 
-  public const System.String WELL_KNOWN_LE = "LetsEncrypt";
-  public const System.String WELL_KNOWN_LESTAGE = "LetsEncrypt-STAGING";
+  public const String WELL_KNOWN_LE = @"LetsEncrypt";
+  public const String WELL_KNOWN_LESTAGE = @"LetsEncrypt-STAGING";
 
-  public static readonly IReadOnlyDictionary<System.String, System.String> WELL_KNOWN_BASE_SERVICES =
-          new ReadOnlyDictionary<System.String, System.String> ( new IndexedDictionary<System.String, System.String> {
-           [ WELL_KNOWN_LE ] = "https://acme-v01.api.letsencrypt.org/",
-           [ WELL_KNOWN_LESTAGE ] = "https://acme-staging.api.letsencrypt.org/",
+  public static readonly IReadOnlyDictionary<String, String> WELL_KNOWN_BASE_SERVICES =
+          new ReadOnlyDictionary<String, String> ( new IndexedDictionary<String, String> {
+           [ WELL_KNOWN_LE ] = @"https://acme-v01.api.letsencrypt.org/",
+           [ WELL_KNOWN_LESTAGE ] = @"https://acme-staging.api.letsencrypt.org/",
           } );
 
   [Parameter ( ParameterSetName = PSET_BASE_SERVICE )]
-  [ValidateSet (
-          WELL_KNOWN_LE,
-          WELL_KNOWN_LESTAGE,
-          IgnoreCase = true )]
-  public System.String BaseService { get; set; } = WELL_KNOWN_LE;
+  [ValidateSet ( WELL_KNOWN_LE, WELL_KNOWN_LESTAGE, IgnoreCase = true )]
+  public String BaseService { get; set; } = WELL_KNOWN_LE;
 
   [Parameter ( ParameterSetName = PSET_BASE_URI, Mandatory = true )]
   [ValidateNotNullOrEmpty]
-  public System.String BaseUri { get; set; }
+  public String BaseUri { get; set; }
 
   [Parameter]
   public SwitchParameter Force { get; set; }
 
   [Parameter]
-  public System.String Alias { get; set; }
+  public String Alias { get; set; }
 
   [Parameter]
-  public System.String Label { get; set; }
+  public String Label { get; set; }
 
   [Parameter]
-  public System.String Memo { get; set; }
+  public String Memo { get; set; }
 
   [Parameter]
-  public System.String VaultProfile { get; set; }
+  public String VaultProfile { get; set; }
 
   protected override void ProcessRecord () {
    var baseUri = BaseUri;
-   if ( System.String.IsNullOrEmpty ( baseUri ) ) {
-    if ( !System.String.IsNullOrEmpty ( BaseService )
-            && WELL_KNOWN_BASE_SERVICES.ContainsKey ( BaseService ) ) {
+   if ( String.IsNullOrEmpty ( baseUri ) ) {
+    if ( !String.IsNullOrEmpty ( BaseService ) && WELL_KNOWN_BASE_SERVICES.ContainsKey ( BaseService ) ) {
      baseUri = WELL_KNOWN_BASE_SERVICES[ BaseService ];
      WriteVerbose ( $"Resolved Base URI from Base Service [{baseUri}]" );
     } else {
-     throw new PSInvalidOperationException ( "either a base service or URI is required" );
+     throw new PSInvalidOperationException ( @"Either a base service or URI is required" );
     }
    }
 
    using ( var vlt = Util.VaultHelper.GetVault ( VaultProfile ) ) {
-    WriteVerbose ( "Initializing Storage Backend" );
+    WriteVerbose ( @"Initializing Storage Backend" );
     vlt.InitStorage ( Force );
     var v = new VaultInfo {
      Id = EntityHelper.NewId (),
